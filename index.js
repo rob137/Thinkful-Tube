8c06ba41596339effb12df9c5edbe5b00595aa54
Robert Kirby <robertaxelkirby@gmail.com>
2017-12-11 14:22:53 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+let linkWithoutSearchTerm = https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAwrO9kTCuS6Zimy4p4zCZMa-UUsgJ_7OU&q=;
+searchTerm = !Whatever user puts in search box!;
+const linkFull = linkWithoutSearchTerm + searchTerm;
+
+function getVideoSearchData() {
+	
+}
+
+function generateHtml() {
+	
+}
+
+function loadHtml() {
+	
+}
+
+json.items[num].
+
+
+
+
+
+
+{
+	"kind": "youtube#searchResult",
+	"etag": "\"7991kDR-QPaa9r0pePmDjBEa2h8/hTo46WSVchv8GoQNeiMgCeaQn0I\"",
+	"id": {
+		"kind": "youtube#video",
+		"videoId": "SfLV8hD7zX4"
+	},
+	"snippet": {
+		"publishedAt": "2017-01-21T00:15:19.000Z",
+		"channelId": "UCE0uCIsTkr2IKyLB10ghiRQ",
+		"title": "Jealous Dog Want Attention Compilation NEW",
+		"description": "We all know our pets have feelings, and our dogs sometimes exhibit signs of jealousy. But are they really jealous? Feel free to leave a comment! Share this video in Facebook/Twitter/Google+...",
+		"thumbnails": {
+			"default": {
+				"url": "https://i.ytimg.com/vi/SfLV8hD7zX4/default.jpg",
+				"width": 120,
+				"height": 90
+			},
+			"medium": {
+				"url": "https://i.ytimg.com/vi/SfLV8hD7zX4/mqdefault.jpg",
+				"width": 320,
+				"height": 180
+			},
+			"high": {
+				"url": "https://i.ytimg.com/vi/SfLV8hD7zX4/hqdefault.jpg",
+				"width": 480,
+				"height": 360
+			}
+		},
+		"channelTitle": "Sad Cat",
+		"liveBroadcastContent": "none"
+	}
+},
+
+
+
+*/
+
+
+
