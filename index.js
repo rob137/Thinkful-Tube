4528548f8cb41afe2a293403550c1a44e7cfac61
Robert Kirby <robertaxelkirby@gmail.com>
2017-12-12 08:57:37 +0000

    testing buttons

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ function handleSearchFormSubmission() {
 	$("form").submit(event, function(event) {
 		event.preventDefault();
 		//Get the user's query and use it to create a YouTube API link for json request
-		const query = prepareQuery();
-		fetchJsonAndLoadHtml(query);	
+		const queryWithSearchTerms = prepareQuery();
+		fetchJsonAndRenderHTML(queryWithSearchTerms);
+		listenForThumbnailClick();
 	});
 }
 
@@ -15,19 +16,26 @@ function prepareQuery() {
 	console.log('prepareQuery() ran.');
 	// Just need to add the user's query onto the end of this link...
 	const queryWithoutSearchTerms = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAwrO9kTCuS6Zimy4p4zCZMa-UUsgJ_7OU&q=';
-	const querySearchTerms = $(".js-query").val(); 
-	const queryFinal = queryWithoutSearchTerms + querySearchTerms;
-	// ... Giving us a link we can use for json quests to the YouTube API.
-	return queryFinal;
+	const searchTerms = $(".js-query").val(); 
+	const queryWithSearchTerms = queryWithoutSearchTerms + searchTerms;
+	// ... Giving us a link we can use for json requests to the YouTube API.
+	return queryWithSearchTerms;
 }
 
-// This function gets the json and load the relevant results into the html. 
-function fetchJsonAndLoadHtml(query) {
-	let searchResult = '';
+// This function gets the json and loads the results html into the div class 'js-search-results'. 
+function fetchJsonAndRenderHTML(query) {
+	console.log('fetchJsonAndRenderHTML() ran.');
 	$.getJSON(query, function(json) {
-		let html = '';
-		for (let item in json.items) {
+		generateResultsHtml(json, query);
+		generateNextPreviousButtons(json, query);
+		// Load the new html into the empty .js-search-results div!
+	});
+}
 
+function generateResultsHtml(json, query) {
+	console.log('generateResultsHtml() ran.');
+	let resultsHtml = '';
+	for (let item in json.items) {
 		// For H2 elements
 		const videoTitle = json.items[item].snippet.title;
 		// For accompanying img elements
@@ -35,17 +43,48 @@ function fetchJsonAndLoadHtml(query) {
 		// Gets the unique identifier for the YouTube video.  Can be 
 		// used for all sorts of things (see next comment).
 		const videoId = json.items[item].id.videoId;
-		// For example: 
+		// Video channel for link to display beneath video
+		const channelId = json.items[item].snippet.channelId;
+		// 
+		
 		let videoUrl = 'https://www.youtube.com/watch/' + videoId;
-		html+= `<h3>${videoTitle}</h3>
-					<img class="thumbnail" id="${videoId}" src="${videoThumbnail}"></img><br><br>`;
-		};
-		// Load the new html into the empty .js-search-results div!
-		$(".js-search-results").html(html);	
-	});
-	// see next comment!
-	listenForThumbnailClick();
+		resultsHtml+= `<h3>${videoTitle}</h3>
+				<img class="thumbnail" id="${videoId}" src="${videoThumbnail}"></img>
+				<p class="channel-text">Click <a href="https://www.youtube.com/channel/${channelId}">here</a> to see this video's channel<br><br>`;
+	};
+	$(".js-search-results").html(resultsHtml);	
 }
+
+function generateNextPreviousButtons(json, query) {
+	console.log('generateNextPreviousButtons() ran.');
+	let nextPreviousButtonsHtml = ''; 
+	if (json.prevPageToken) {
+		nextPreviousButtonsHtml += `<button class="next-previous-button previous-button">Previous</button>`
+	}; 
+	nextPreviousButtonsHtml+=`<button class="next-previous-button next-button">Next</button>`
+	
+	$('.js-next-previous').html(nextPreviousButtonsHtml);
+
+	listenForNextOrPreviousClick(json);
+}
+
+function listenForNextOrPreviousClick(json, query) {
+	console.log('listenForNextOrPreviousClick() ran.');
+	$('.next-button').click(event, function(event) {
+		event.stopPropagation();
+		const nextPageQuery = '&pageToken=' + json.nextPageToken;
+		query += nextPageQuery;
+		fetchJsonAndRenderHTML(query);
+	});
+}
+
+
+
+
+
+
+
+
 
 // Listen out for clicks on thumbnails.  This will be the user prompt to 
 // display 'lightbox' embedded videos.
@@ -74,7 +113,7 @@ function showLightbox(target) {
 }
 
 // Reverses the visual effects described in the description for 
-// showLightbox().
+// showLightbox().  Also stops the video.
 function handleCloseClick() {
 	console.log('handleCloseClick() ran.');
 	$('.close').click(event, function(event) {
@@ -82,6 +121,9 @@ function handleCloseClick() {
 		$('.lightbox-parent').addClass('hidden');
 		$('.lightbox-shade').removeClass('lightbox-on');
 		$('form, img').removeClass('opaque');
+		// Removes the video link from the lightbox, causing buffering and video
+		// play to stop when the user clicks 'close video'.
+		$('.lightbox').attr('src', "");
 	})
 }
 
